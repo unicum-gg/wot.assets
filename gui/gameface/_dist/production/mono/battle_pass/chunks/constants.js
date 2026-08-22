2ea4a705a64301614c137a4fe7e437d3d5b83e88
wot.build <actions@users.noreply.github.com>
2026-08-22 21:09:39 +0000

    🍱 update WG assets to 2.3.1.5412

--- gui/gameface/_dist/production/mono/battle_pass/chunks/constants.js
+++ gui/gameface/_dist/production/mono/battle_pass/chunks/constants.js
@@ -1,3 +1,1 @@
-const s = 3800,
-    a = 'new_skill';
-export { a as N, s as T };
+const s=3800,a="new_skill";export{a as N,s as T};
--- gui/gameface/_dist/production/mono/user_missions/views/tooltips/daily_reroll_tooltip/daily_reroll_tooltip.html/bundle.js
+++ gui/gameface/_dist/production/mono/user_missions/views/tooltips/daily_reroll_tooltip/daily_reroll_tooltip.html/bundle.js
@@ -1,17 +1,1 @@
-import { s as r, j as s } from '../../../../chunks/vendor.js';
-import { i as e, n as i, r as t, f as o, s as l, c as a } from '../../../../chunks/lib.js';
-import { E as n } from '../../../../chunks/extended_tooltip_decorator.js';
-/* empty css                       */ import '../../../../chunks/divider.js';
-const [m, d] = e()(({ observableModel: r }) => ({ ...r.primitives(['timeLeft', 'rerollInterval']) }), i),
-    c = r(function () {
-        const { model: r } = d(),
-            e = t.resolve('strings'),
-            [i] = o(l(r.rerollInterval.get()), ['h']);
-        return s.jsx(n, {
-            header: e.readOrEmpty('user_missions.tooltip.daily_reroll.header'),
-            description: e.readOrEmpty('user_missions.tooltip.daily_reroll.description'),
-            descriptionParams: { time: i },
-            timerTimeLeft: r.timeLeft.get(),
-        });
-    });
-a(s.jsx(m, { children: s.jsx(c, {}) }));
+import{s as r,j as e}from"../../../../chunks/vendor.js";import{i as s,n as i,b as t,f as o,d as l,r as a}from"../../../../chunks/lib.js";import{E as n}from"../../../../chunks/extended_tooltip_decorator.js";/* empty css                       */import"../../../../chunks/divider.js";const[m,d]=s()(({observableModel:r})=>({...r.primitives(["timeLeft","rerollInterval"])}),i),p=r(function(){const{model:r}=d(),s=t.resolve("strings"),[i]=o(l(r.rerollInterval.get()),["h"]);return e.jsx(n,{header:s.readOrEmpty("user_missions.tooltip.daily_reroll.header"),description:s.readOrEmpty("user_missions.tooltip.daily_reroll.description"),descriptionParams:{time:i},timerTimeLeft:r.timeLeft.get()})});a(e.jsx(m,{children:e.jsx(p,{})}));
